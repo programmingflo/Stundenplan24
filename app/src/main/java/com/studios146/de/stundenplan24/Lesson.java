24b1a13f1049b55b403b0e0aba009e632d72288b
programmingflo <flori08mans@gmail.com>
2017-07-04 14:39:38 +0200

    added Boolean substituted

--- app/src/main/java/com/studios146/de/stundenplan24/Lesson.java
+++ app/src/main/java/com/studios146/de/stundenplan24/Lesson.java
@@ -14,6 +14,7 @@ class Lesson {
     String lehrer;
     String raum;
     String info;
+    Boolean substituted;
 
     /**
      * @param id ID
@@ -24,9 +25,10 @@ class Lesson {
      * @param lehrer Lehrer
      * @param raum Raum
      * @param info Info
+     * @param substituted Boolean, that returns, whether lesson is substituted
      */
     Lesson(long id, String klasse, String tag, String stunde, String fach,
-           String lehrer, String raum, String info) {
+           String lehrer, String raum, String info, Boolean substituted) {
         this.id = id;
         this.klasse = klasse;
         this.tag = tag;
@@ -35,11 +37,12 @@ class Lesson {
         this.lehrer = lehrer;
         this.raum = raum;
         this.info = info;
+        this.substituted = substituted;
     }
 
     @Override
     public String toString(){
         return this.id+","+this.klasse+","+this.tag +","+this.stunde+","+this.fach+","+this.lehrer+","+
-                this.raum+","+this.info;
+                this.raum+","+this.info+","+this.substituted;
     }
 }
